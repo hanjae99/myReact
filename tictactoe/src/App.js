f3b8c4291e7624e92f1eb156e74afc67642a683d
hanjae <leehanjae99@gmail.com>
2023-08-15 17:38:43 +0900

    React-tictactoe-230815

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -9,17 +9,31 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({ squares, isOnext, onPlay }) {
   function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
     const newSquares = [...squares];
-    newSquares[i] = "X";
-    setSquares(newSquares);
+    if (isOnext) {
+      newSquares[i] = "O";
+    } else if (!isOnext) {
+      newSquares[i] = "X";
+    }
+    onPlay(newSquares);
+  }
+
+  let status;
+  const winner = calculateWinner(squares);
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (isOnext ? "O" : "X");
   }
 
   return (
     <>
+      <h1>{status}</h1>
       <div className="board-row">
         <Square
           value={squares[0]}
@@ -66,12 +80,73 @@ function Board() {
   );
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 function App() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  // const [isOnext, setIsOnext] = useState(false);
+  const isOnext = currentMove % 2 !== 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(newSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), newSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li key={move}>
+        <button
+          onClick={() => {
+            jumpTo(move);
+          }}
+        >
+          {description}
+        </button>
+      </li>
+    );
+  });
+
   return (
-    <>
-      <h1>Winner: 한재</h1>
-      <Board />
-    </>
+    <div className="game">
+      <div className="game-board">
+        <Board squares={currentSquares} isOnext={isOnext} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
   );
 }
 
